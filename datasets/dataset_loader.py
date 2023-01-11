e6e048d67ec418407c60ad9f95d3a3ec47935796
Mathias Claassen <mathias@xmartlabs.com>
2023-01-11 17:06:40 -0300

    Add notebooks and unify for different datasets

--- datasets/dataset_loader.py
+++ datasets/dataset_loader.py
@@ -1,4 +1,7 @@
 
+import os
+
+
 class DatasetLoader():
     """Abstract class that serves to load datasets from different sources (local, ClearML, other tracker)
     """
@@ -10,4 +13,5 @@ class DatasetLoader():
 class LocalDatasetLoader(DatasetLoader):
 
     def get_dataset_folder(self, dataset_project, dataset_name):
-        return f"data/{dataset_name}"
+        base_folder = os.environ.get("BASE_DATA_FOLDER", "data")
+        return os.path.join(base_folder, dataset_name)
